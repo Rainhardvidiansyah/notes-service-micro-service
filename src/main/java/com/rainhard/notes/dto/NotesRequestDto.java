eb8b34388c9151279ffe864381d15c8a8f4650b3
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-05-29 03:17:42 +0700

    create notes dto class

---
+++ src/main/java/com/rainhard/notes/dto/NotesRequestDto.java
@@ -0,0 +1,30 @@
+package com.rainhard.notes.dto;
+
+
+public class NotesRequestDto {
+
+    private String title;
+    private String content;
+
+    
+    public NotesRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
