7c414e090ef560bf9613b0039b0ce5371794b63e
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-05-30 03:14:18 +0700

    Add and edit query and methods in notes repository

--- src/main/java/com/rainhard/notes/repository/NotesRepository.java
+++ src/main/java/com/rainhard/notes/repository/NotesRepository.java
@@ -1,10 +1,12 @@
 package com.rainhard.notes.repository;
 
 import com.rainhard.notes.entity.Notes;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +21,11 @@ public interface NotesRepository extends CrudRepository<Notes, Long> {
     @Query("SELECT * FROM notes WHERE id = :id")
     Optional<Notes> findOneNote(@Param("id") Long id);
 
-    @Query("SELECT * FROM notes WHERE title = :title")
-    String getNotesFromTitle(@Param("title") String title);
+    @Query("SELECT * FROM notes  WHERE title = :title")
+    List<Notes> getNotesFromTitle(@Param("title") String title);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE notes SET title = :title, content= :content WHERE id = :id")
+    int updateTitleAndContent(@Param("id") Long id, @Param("title") String title, @Param("content") String content);
 }
