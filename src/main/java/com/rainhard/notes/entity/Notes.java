827bf199c95c034c0922564c914fff03adc84b96
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-05-30 00:24:18 +0700

    Add getter setter in Note class

--- src/main/java/com/rainhard/notes/entity/Notes.java
+++ src/main/java/com/rainhard/notes/entity/Notes.java
@@ -28,4 +28,55 @@ public class Notes {
     @LastModifiedDate
     @Column("updatedAt")
     private LocalDateTime updatedAt;
+
+    public Notes() {
+    }
+
+    public Notes(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
