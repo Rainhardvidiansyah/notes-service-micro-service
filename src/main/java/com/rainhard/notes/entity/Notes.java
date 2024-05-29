b166eda02114e094b006284456133c74b7604496
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-05-30 01:55:09 +0700

    Change body of empty parameter in Notes class

--- src/main/java/com/rainhard/notes/entity/Notes.java
+++ src/main/java/com/rainhard/notes/entity/Notes.java
@@ -30,14 +30,14 @@ public class Notes {
     private LocalDateTime updatedAt;
 
     public Notes() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
-    public Notes(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public Notes(Long id, String title, String content) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 
     public Long getId() {
