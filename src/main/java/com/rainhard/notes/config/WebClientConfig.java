6eb67078d67b70943921d812c10152227a5e4753
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-06-01 00:51:48 +0700

    Make a web client bean

--- src/main/java/com/rainhard/notes/config/WebClientConfig.java
+++ src/main/java/com/rainhard/notes/config/WebClientConfig.java
@@ -1,8 +1,17 @@
 package com.rainhard.notes.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 
 public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().build();
+    }
 
 
 }
