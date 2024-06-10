f481367fb4d1bb7d8fe30cd63086040cf0961017
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-06-11 01:35:33 +0700

    create two new methods to test and compare which best library to consume api

--- src/main/java/com/rainhard/notes/controller/NotesController.java
+++ src/main/java/com/rainhard/notes/controller/NotesController.java
@@ -3,6 +3,7 @@ package com.rainhard.notes.controller;
 
 import com.rainhard.notes.dto.NotesRequestDto;
 import com.rainhard.notes.entity.Notes;
+import com.rainhard.notes.service.ApiService;
 import com.rainhard.notes.service.NotesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -21,6 +25,9 @@ public class NotesController {
 
     @Autowired
     private NotesService notesService;
+
+    @Autowired
+    private ApiService apiService;
 
     @PostMapping("/")
     public ResponseEntity<?> createNote(@RequestBody NotesRequestDto notesRequestDto){
@@ -39,14 +46,39 @@ public class NotesController {
         return new ResponseEntity<>(notes, HttpStatusCode.valueOf(200));
     }
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public List<Notes> getNotes(){
         log.info("Get all notes in getNotes method Notes Controller");
-        return this.notesService.getNotes();
+        return this.notesService.getAllNotesJdbc();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getNoteById(@PathVariable Long id){
+        return new ResponseEntity<>(responses(this.notesService.findNoteById(id)),
+                HttpStatusCode.valueOf(200));
     }
 
     private <T> T responses(T t){
         return t;
+    }
+
+    @GetMapping("/testing")
+    public Mono<ResponseEntity<Object>> getData(){
+        var s = apiService.getData();
+        System.out.println(s);
+        return s.map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/testing/http")
+    public ResponseEntity<?> getDataHttp(){
+        try {
+            var s = apiService.getDataHttp();
+            return new ResponseEntity<>(s, HttpStatusCode.valueOf(200));
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatusCode.valueOf(500));
     }
 
 }
